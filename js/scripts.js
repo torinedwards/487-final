2f6158ce4777aac18d0a73d463c37eea0e2d315d
torin <torin@live.unc.edu>
2018-12-11 19:34:43 -0500

    Looped through the JSON data

--- js/scripts.js
+++ js/scripts.js
@@ -10,10 +10,55 @@ $(document).ready(function() {
            };
 function test() {
   if (student.likesCoding) {
-    console.log(student.name, student.age)
+    console.log(student.name, student.age);
+  }
+}
+test();
+
+// Set up any variables needed
+var url = './js/refugees.json';
+var origin = [];
+var value = [];
+
+// Load the JSON data
+$.ajax({
+  type: 'GET',
+  url: url,
+  async: true,
+  success: function(data){
+    // Console log the data
+    console.log(data.length);
+
+    // Callbacks for logic and chart
+    buildContent(data);
+    // buildChart(data);
+  },
+
+  error: function(error) {
+    console.log(error);
+  },
+});
+
+// Function to do logic
+function buildContent(data) {
+  // Set up a for loop to loop through the data
+  for(i = 0; i < data.length; i++) {
+    // Push data to different arrays to prepare for chart
+    origin.push(data[i].Origin);
+
+    value.push(data[i].Value);
+    $( ".cards" ).append( "<div class='card" + i + "'" + "></div>" );
+    $( ".card" + i ).append( "<h1>"+origin[i]+"</h1>" );
+    $( ".card" + i ).append( "<h1>"+value[i]+"</h1>" );
+
+  }
+  for(i = 0; i < data.length; i++) {
   }
 }
 
+console.log(origin);
+console.log(value);
 
-test();
+
+
   });
